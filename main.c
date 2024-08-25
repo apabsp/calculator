0e2847f36fa498b2e910c12aaef7fbd85dbaeea8
Antonio Paulo Barros <95260401+apabsp@users.noreply.github.com>
2024-08-25 16:06:56 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
-//Antonio Paulo Araujo de Barros e Silva - apabs - 21/08/2024 22:17
-// Calculando base 2 e função de inverter feito 
-
+//Antonio Paulo Araujo de Barros e Silva - apabs 
+// Calculando base 2 e função de inverter feito - 21/08/2024 22:17
+// Calculando Base 8, Base 16 e BCD - 25/08/2024 15:55
+//TODO: -complemento a 2;
+//     -converter real em decimal para float e double, mostrando os respectivos bits de sinal , expoente, expoente com viés e fração 
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -64,6 +66,61 @@ void calculandoBase2(float paraConverter, char answer[50]) {
   printf("Resposta é: %s", Invert(answer));
 }
 
+void calculandoBase8(float paraConverter, char answer[50]) {
+  char myNewString[50];
+  int intParaConverter = (int)paraConverter;
+  int i = 0;
+  while (intParaConverter != 0) {
+      myNewString[i] = (intParaConverter % 8) + '0';
+      printf("%d %% 8 = %d\n", intParaConverter, intParaConverter % 8);
+      intParaConverter = intParaConverter / 8;
+      i++;
+  }
+  myNewString[i] = '\0';
+  strcpy(answer, myNewString);
+  printf("Resposta é: %s\n", Invert(answer));
+}
+
+  //  BC    
+void calculandoBase16(float paraConverter, char answer[50]) {
+  char myNewString[50];
+  int intParaConverter = (int)paraConverter;
+  int i = 0;
+  while (intParaConverter != 0) {
+      int restante = intParaConverter % 16;
+    
+      myNewString[i] = (restante < 10) ? (restante + '0') : (restante - 10 + 'A'); // lets use ASCII to our advantage
+    // 3 3 + '0'
+      printf("%d %% 16 = %d\n", intParaConverter, restante);
+    
+      intParaConverter = intParaConverter / 16;
+      i++;
+  }
+  myNewString[i] = '\0';
+  strcpy(answer, myNewString);
+  printf("Resposta é: %s\n", Invert(answer));
+}
+
+//binary coded decimal! like 0 = 0000, 2 = 0010, 3 = 0011
+void calculandoBCD(float paraConverter, char answer[50]) {
+  char myNewString[50];
+  int intParaConverter = (int)paraConverter;
+  int i = 0;
+  int digito;
+  
+  while (intParaConverter != 0){
+    digito = intParaConverter % 10;
+    for (int j = 0; j < 4; j++){
+      myNewString[i++] = (digito % 2) ? '1' : '0';
+    }
+      intParaConverter = intParaConverter / 10;
+  }
+  myNewString[i] = '\0';
+  strcpy(answer, myNewString);
+  printf("Resposta em BCD é: %s\n", Invert(answer));
+
+  
+}
 
 
 int main(void) {
@@ -80,14 +137,25 @@ int main(void) {
   while (continuar != 'N' || continuar != 'n') {
     printf("Digite o número: ");
     scanf("%f", &numeroParaCalcular);
-    printf("Escolha a opção: ");
+    printf("Escolha a opção: \n 1-Para binário\n 2-Para base octo\n 3-Para base 16\n 4- Para BCD \n \n");
     scanf("%d", &opcao);
 
     switch (opcao) {
-    case 1: // binary
-      calculandoBase2(numeroParaCalcular, answer);
+      case 1: // binary
+        calculandoBase2(numeroParaCalcular, answer);
+        break;
+      case 2:
+        calculandoBase8(numeroParaCalcular, answer);
+        break;
+      case 3:
+        calculandoBase16(numeroParaCalcular, answer);
+        break;
+      case 4:
+        calculandoBCD(numeroParaCalcular, answer);
+        break;
     }
   }
 
   return 0;
 }
+
